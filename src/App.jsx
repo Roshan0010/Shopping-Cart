72ab08a5b0858ba420a047cf49fc53c1cdc16349
Roshan0010 <iamroshan6417@gmail.com>
2023-12-01 01:13:17 +0530

    g

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
     <Routes >
       <Route path="/" element={<Home/>}/>
       <Route path="/cart" element={<Cart/>} />
-      <Route path="/Shopping-Cart" element={<Home/>} />
+      <Route path="/Shopping-Cart/" element={<Home/>} />
     </Routes>
     </div>
     
